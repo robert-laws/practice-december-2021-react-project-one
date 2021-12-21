d76cc7560fa4f77c40a2739900160e48c0422873
Robert Laws <roblaws@me.com>
2021-12-21 16:06:25 +0300

    starting point setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './sass/App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Matching Game</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
